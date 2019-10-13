e45ead688b8ad104bb9e6dfec3943c32e28941da
Farnerud <hperea.alfa@gmail.com>
2019-10-12 22:28:18 -0500

    add router links and images with its corresponding styles.

--- src/router.js
+++ src/router.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Brazil from "./views/Brazil.vue";
+import Jamaica from "./views/Jamaica.vue";
+import Panama from "./views/Panama.vue";
+import Hawaii from "./views/Hawaii.vue";
 
 Vue.use(Router);
 
@@ -19,6 +23,26 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/brazil",
+      name: "brazil",
+      component: Brazil
+    },
+    {
+      path: "/jamaica",
+      name: "jamaica",
+      component: Jamaica
+    },
+    {
+      path: "/panama",
+      name: "panama",
+      component: Panama
+    },
+    {
+      path: "/hawaii",
+      name: "hawaii",
+      component: Hawaii
     }
   ]
 });
